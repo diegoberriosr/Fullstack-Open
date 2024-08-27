cf81179c465832caf44620ebe8862e7c397a1af4
Diego Berrios <diegus99@gmail.com>
2024-08-27 10:29:08 -0400

    added .env file

--- Part2/countries/src/services/weather.js
+++ Part2/countries/src/services/weather.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const KEY = 'f9825d9ad0dd0ee82dcf0e0502bd95e9'
+const KEY = import.meta.env.KEY
 const getWeather = async (lat, lon) => {
     return await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${KEY}`) 
 }
