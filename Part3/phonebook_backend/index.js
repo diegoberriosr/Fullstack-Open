3769d74f9983816f11a74a8133d1ea33e57a6e2f
Diego Berrios <diegus99@gmail.com>
2024-08-27 14:04:38 -0400

    Added dist folder

--- Part3/phonebook_backend/index.js
+++ Part3/phonebook_backend/index.js
@@ -11,7 +11,7 @@ morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
 
 app.use(cors())
-
+app.use(express.static('dist'))
 let people = [
     { 
       "id": "1",
